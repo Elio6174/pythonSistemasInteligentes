b439ff0be2f8bb3b3d9f8b51b33066f2eef40808
Elio6174 <elseismylife@gmail.com>
2026-03-18 16:50:23 -0600

    modelo de aprendizaje de OR

--- Neuronas/perceptron.py
+++ Neuronas/perceptron.py
@@ -1,21 +1,27 @@
 import random
-
+#algoritmo de red neuronal OR
 def perceptron(patrones, w, umbral):
-    for patron in patrones:
-        #Calcular FX
-        fx=patron[0]*w[0]+patron[1]*w[1]+umbral
+    while(True):
+        bandera = True
+        for patron in patrones:
+            #Calcular FX
+            fx=patron[0]*w[0]+patron[1]*w[1]+umbral
 
-        #Calcular la funcion de activacion (y)
-        if fx>0:
-            y = 1
-        else:
-            y -=1
-        #Evaluar si y!=d(x) y realizar ajustes
-        if y!=patron[2]:
-            #ajustamos pesos y umbral
-            w[0]=w[0]+patron[2]*patron[0]
-            w[1]=w[1]+patron[2]*patron[1]
-            umbral=umbral + patron[2]
+            #Calcular la funcion de activacion (y)
+            if fx>0:
+                y = 1
+            else:
+                y -=1
+            #Evaluar si y!=d(x) y realizar ajustes
+            if y!=patron[2]:
+                #ajustamos pesos y umbral
+                w[0]=w[0]+patron[2]*patron[0]
+                w[1]=w[1]+patron[2]*patron[1]
+                umbral=umbral + patron[2]
+        if bandera == True:
+            break
+    return w,umbral
+
 
 #Fase de entrenamiendo
 #1.-Patrones de entrada compuerta OR
@@ -24,4 +30,14 @@ umbral=random.uniform(-1,1)
 w=[]
 w.append(random.uniform(-1,1))
 w.append(random.uniform(-1,1))
-w,umbral=perceptron(patrones,w,umbral)
+w,umbral=perceptron(patrones,w,umbral)  
+
+#Fase de evaluacion/prediccion
+x1=int(input("Ingresa el valor para x1="))
+x2=int(input("Ingresa el valor para x2="))
+prediccion=x1*w[0]+x2*w[1]+umbral
+
+if prediccion>0:
+    print("Verdadero")
+else:
+    print("False")
